934691177f10ab3bb479c41de5cee44e989d98b5
Kousa Mika <mika.kousa@gmail.com>
2016-06-01 17:03:24 +0300

    More callbacks.

--- nodejs/net-tcp-client.js
+++ nodejs/net-tcp-client.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
+/*
+ testing
+ nc localhost 5555
+ nc -z localhost 5555
+*/
+
 var os=require('os')
 var net = require('net');
-console.log("net:");
-console.log(net);
+//console.log("net:");
+//console.log(net);
 
 function connect_cb() {
     console.log("connect cb");
+    //console.log(clientsock);
+    clientsock.write("ABC");
 }
 
 opts = { host: "localhost",
@@ -15,6 +23,14 @@ opts = { host: "localhost",
 var clientsock = net.connect(opts);
 
 clientsock.on('connect', connect_cb);
+clientsock.on('end', function (x) {
+    console.log("end cb,"+x);
+    //console.log(clientsock);
+});
+clientsock.on('finish', function (x) {
+    console.log("finish cb,"+x);
+    //console.log(clientsock);
+});
 
-console.log("clientsock:");
-console.log(clientsock);
+//console.log("clientsock:");
+//console.log(clientsock);
--- nodejs/net-tcp-server.js
+++ nodejs/net-tcp-server.js
@@ -2,23 +2,30 @@
 
 var os = require('os');
 var net = require('net');
-console.log("net:");
-console.log(net);
+//console.log("net:");
+//console.log(net);
 
 function listen_cb() {
     console.log("listen cb");
 }
 
 function connection_cb(socket) {
-    console.log("connection cb, socket:", socket);
+    client = socket;
+    //console.log("connection cb, socket:", socket);
+    console.log("connection cb");
     socket.write(Date() + " uptime=" + os.uptime() + "\n");
     server.getConnections(function (err, count) {
         if (err) {
-            console.log(err);
+            console.log("getConnections, err=" + err);
         } else {
             socket.write("getConnections=" + count + "\n");
         }
     });
+
+    socket.on('data', socket_data_cb);
+    socket.on('close', socket_close_cb);
+    socket.on('error', socket_error_cb);
+    //server.close(close_cb);
 }
 
 function close_cb() {
@@ -26,7 +33,24 @@ function close_cb() {
 }
 
 function error_cb(err) {
-    console.log("error cb, err:" + err);
+    console.log("error cb");
+    console.log(err);
+}
+
+function socket_data_cb(data) {
+    console.log("socket data cb:");
+    console.log(data);
+    if (client)
+        client.write("len=" + data.length +"\n");
+}
+
+function socket_close_cb(err) {
+    console.log("socket close cb, err=" + err);
+}
+
+function socket_error_cb(err) {
+    console.log("socket error cb");
+    console.log(err);
 }
 
 var server = net.createServer();
@@ -34,5 +58,5 @@ server.on('connection', connection_cb);
 server.on('close', close_cb);
 server.on('error', error_cb);
 server.listen(5555, listen_cb);
-console.log("server:");
-console.log(server);
+//console.log("server:");
+//console.log(server);
